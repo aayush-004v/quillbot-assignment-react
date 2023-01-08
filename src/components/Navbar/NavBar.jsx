8555fc7efa82f1e5f5fb50ca2ea861a5fe409887
AAYUSH VERMA (avi) <106806750+aayush-004v@users.noreply.github.com>
2023-01-09 01:10:29 +0530

    Update NavBar.jsx

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React from 'react';
+import quillbotIcon from '../../assets/quillbotIcon.jpg';
+import './NavBarStyle.css';
 
 const NavBar = () => {
     return (
-        <div>
-            <nav className="navbar fixed-top bg-white m-0 p-0 style={{border: '2px solid black'}}">
+        <div className='main__navBar'>
+            <nav className="navbar fixed-top bg-white m-0 p-0">
                 <div className="border border-2 container-fluid d-flex text-center">
-                    <a className="navbar-brand" href="https://quillbot.com/premium?utm_medium=paid_search&utm_source=google&utm_campaign=sitelinks&campaign_type=extension">
-                        <img src="https://assets.stickpng.com/images/630f865ad5353300f4bd3291.png" alt="QuillBot" width="150px" height="50px" className="d-inline-block align-text-top" target="_blank" />
+                    <a className="navbar-brand" href="" >
+                        <img src={quillbotIcon} alt="QuillBot" className="align-text-top image__tag" target="_blank" />
                     </a>
-                    <h3 className="text-center">QuillBot Premium</h3>
+                    <div className="text-center quillbot-head">QuillBot Premium</div>
                 </div>
             </nav>
         </div>
